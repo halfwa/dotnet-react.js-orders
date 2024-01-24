8ed51dd66f3ab72a70398c16a12659ce189aae75
Yaroslav Shamov <yaroslav.shamov2@mail.ru>
2024-01-24 11:56:32 +0300

    Add  handling data layer exceptions

--- backend/OrderCreator.DataAccess/Repositories/OrdersRepository.cs
+++ backend/OrderCreator.DataAccess/Repositories/OrdersRepository.cs
@@ -1,91 +1,133 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using OrderCreator.Core.Abstractions;
 using OrderCreator.Core.Models;
 using OrderCreator.DataAccess.Entities;
 
 namespace OrderCreator.DataAccess.Repositories
 {
-    public sealed class OrdersRepository(OrderCreatorDbContext _context) : IOrdersRepository
+    public sealed class OrdersRepository(
+        OrderCreatorDbContext _context,
+        ILogger _logger
+        ) : IOrdersRepository
     {
         public async Task<Order> GetById(Guid id, CancellationToken cancellationToken)
         {
-            var orderEntity = await _context.Orders
+            try
+            {
+                var orderEntity = await _context.Orders
                 .AsNoTracking()
                 .FirstOrDefaultAsync(o => o.Id == id, cancellationToken);
 
-            var order = Order.Create(
-                orderEntity!.Id,
-                orderEntity.FromCity, orderEntity.FromAddress,
-                orderEntity.ToCity, orderEntity.ToAddress,
-                orderEntity.Weight,
-                orderEntity.PickupDate)
-                    .Order;
+                var order = Order.Create(
+                    orderEntity!.Id,
+                    orderEntity.FromCity, orderEntity.FromAddress,
+                    orderEntity.ToCity, orderEntity.ToAddress,
+                    orderEntity.Weight,
+                    orderEntity.PickupDate)
+                        .Order;
 
-            return order;
+                return order;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "{Repo} GetById function error", typeof(OrdersRepository));
+                throw;
+            }
         }
 
         public async Task<List<Order>> Get(CancellationToken cancellationToken)
         {
-            var orderEntities = await _context.Orders
-                .AsNoTracking()
-                .OrderBy(o => o.PickupDate)
-                .ToListAsync(cancellationToken);
+            try
+            {
+                var orderEntities = await _context.Orders
+                   .AsNoTracking()
+                   .OrderBy(o => o.PickupDate)
+                   .ToListAsync(cancellationToken);
 
-            var orders = orderEntities
-                .Select(o => Order.Create(
-                    o.Id,
-                    o.FromCity, o.FromAddress,
-                    o.ToCity, o.ToAddress,
-                    o.Weight,
-                    o.PickupDate)
-                        .Order)
-                .ToList();
+                var orders = orderEntities
+                    .Select(o => Order.Create(
+                        o.Id,
+                        o.FromCity, o.FromAddress,
+                        o.ToCity, o.ToAddress,
+                        o.Weight,
+                        o.PickupDate)
+                            .Order)
+                    .ToList();
 
-            return orders;
+                return orders;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "{Repo} Get function error", typeof(OrdersRepository));
+                throw;
+            }
         }
 
         public async Task<Guid> Create(Order order, CancellationToken cancellationToken)
         {
-            var orderEntity = new OrderEntity
+            try
             {
-                Id = order.Id,
-                FromCity = order.FromCity,
-                FromAddress = order.FromAddress,
-                ToCity = order.ToCity,
-                ToAddress = order.ToAddress,
-                Weight = order.Weight,
-                PickupDate = order.PickupDate
-            };
+                var orderEntity = new OrderEntity
+                {
+                    Id = order.Id,
+                    FromCity = order.FromCity,
+                    FromAddress = order.FromAddress,
+                    ToCity = order.ToCity,
+                    ToAddress = order.ToAddress,
+                    Weight = order.Weight,
+                    PickupDate = order.PickupDate
+                };
 
-            await _context.Orders.AddAsync(orderEntity, cancellationToken);
-            await _context.SaveChangesAsync(cancellationToken);
+                await _context.Orders.AddAsync(orderEntity, cancellationToken);
+                await _context.SaveChangesAsync(cancellationToken);
 
-            return orderEntity.Id;
+                return orderEntity.Id;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "{Repo} Create function error", typeof(OrdersRepository));
+                throw;
+            }
         }
 
         public async Task<Guid> Update(Guid id, Order order, CancellationToken cancellationToken)
         {
+            try
+            {
+                await _context.Orders
+                 .Where(o => o.Id == id)
+                 .ExecuteUpdateAsync(x => x
+                     .SetProperty(o => o.FromCity, o => order.FromCity)
+                     .SetProperty(o => o.FromAddress, o => order.FromAddress)
+                     .SetProperty(o => o.ToCity, o => order.ToCity)
+                     .SetProperty(o => o.ToAddress, o => order.ToAddress)
+                     .SetProperty(o => o.Weight, o => order.Weight)
+                     .SetProperty(o => o.PickupDate, o => order.PickupDate), cancellationToken);
 
-            await _context.Orders
-                .Where(o => o.Id == id)
-                .ExecuteUpdateAsync(x => x
-                    .SetProperty(o => o.FromCity, o => order.FromCity)
-                    .SetProperty(o => o.FromAddress, o => order.FromAddress)
-                    .SetProperty(o => o.ToCity, o => order.ToCity)
-                    .SetProperty(o => o.ToAddress, o => order.ToAddress)
-                    .SetProperty(o => o.Weight, o => order.Weight)
-                    .SetProperty(o => o.PickupDate, o => order.PickupDate), cancellationToken);
-
-            return id;
+                return id;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "{Repo} Update function error", typeof(OrdersRepository));
+                throw;
+            }
         }
 
         public async Task<Guid> Delete(Guid id, CancellationToken cancellationToken)
         {
-            await _context.Orders
-                .Where(o => o.Id == id)
-                .ExecuteDeleteAsync();
+            try
+            {
+                await _context.Orders.Where(o => o.Id == id)
+                 .ExecuteDeleteAsync();
 
-            return id;
+                return id;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "{Repo} Delete function error", typeof(OrdersRepository));
+                throw;
+            }
         }
     }
 }
